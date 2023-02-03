220b22352461d5b7e6101c9442c0657f752540cf
ALMITA-RUTH <almita.ruth.lu@gmail.com>
2023-02-03 00:38:47 -0600

    se creo el metodo para calcular imc
    se pretende pasar el metodo a la clase operaciones

--- app/src/main/java/com/ruthlink/imc/MainActivity.java
+++ app/src/main/java/com/ruthlink/imc/MainActivity.java
@@ -2,12 +2,29 @@ package com.ruthlink.imc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText peso,altura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        peso = (EditText)findViewById(R.id.txtPeso);
+        altura = (EditText)findViewById(R.id.txtAltura);
+    }
+
+    public void Calcular (View v){
+
+
+        int Resultado, pesot, alturat;
+        pesot = Integer.parseInt((peso.getText().toString()));
+        alturat = Integer.parseInt((altura.getText().toString()));
+        Resultado = pesot + alturat;
+
+        Toast.makeText(this, String.valueOf(Resultado), Toast.LENGTH_SHORT).show();
     }
 }
